6a2cd9bc50606ad3ba9c8a87bf7d7d4349c9a814
Daniel Siepmann <coding@daniel-siepmann.de>
2020-02-13 12:26:43 +0100

    Add rules to prevent tracking of certain requests

--- Classes/Middleware/Pageview.php
+++ Classes/Middleware/Pageview.php
@@ -27,6 +27,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use TYPO3\CMS\Core\Context\Context;
 
 class Pageview implements MiddlewareInterface
 {
@@ -35,17 +37,41 @@ class Pageview implements MiddlewareInterface
      */
     private $repository;
 
-    public function __construct(Repository $repository)
+    /**
+     * @var Context
+     */
+    private $context;
+
+    /**
+     * @var string
+     */
+    private $rule = '';
+
+    public function __construct(Repository $repository, Context $context, string $rule)
     {
         $this->repository = $repository;
+        $this->context = $context;
+        $this->rule = $rule;
     }
 
     public function process(
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
     ): ResponseInterface {
-        $this->repository->add(Factory::fromRequest($request));
+        if ($this->shouldTrack($request, $this->context)) {
+            $this->repository->add(Factory::fromRequest($request));
+        }
 
         return $handler->handle($request);
+    }
+
+    private function shouldTrack(
+        ServerRequestInterface $request,
+        Context $context
+    ): bool {
+        return (bool) (new ExpressionLanguage())->evaluate($this->rule, [
+            'request' => $request,
+            'context' => $context,
+        ]);
     }
 }
