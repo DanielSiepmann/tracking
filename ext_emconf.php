73ec4641ff363aca7f8e468b684b4ded049ee6df
Daniel Siepmann <coding@daniel-siepmann.de>
2020-02-25 19:58:46 +0100

    Prepare usage as standalone composer package

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,19 +3,21 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Tracking',
     'description' => 'Tracks page visits in TYPO3.',
     'category' => 'fe',
-    'state' => 'stable',
+    'state' => 'alpha',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author' => 'Daniel Siepmann',
     'author_email' => 'coding@daniel-siepmann.de',
     'author_company' => '',
-    'version' => '1.0.0',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'core' => '',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            'dashboard' => '',
+        ],
     ],
 ];
