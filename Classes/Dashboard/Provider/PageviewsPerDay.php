0927683926c172219f3cf401e3c1b4eabf19b71f
Daniel Siepmann <coding@daniel-siepmann.de>
2020-04-07 17:54:18 +0200

    Migrate dashboard part to be compatible with v10.4
    10.4 introduced breaking changes for dashboard widgets.
    The necessary changes to stay compatible are done within this commit.
    
    No need anymore to configure widget through PHP, instead DI is used.
    Instead only PHP to provide data is used.

---
+++ Classes/Dashboard/Provider/PageviewsPerDay.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace DanielSiepmann\Tracking\Dashboard\Provider;
+
+/*
+ * Copyright (C) 2020 Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use DanielSiepmann\Tracking\Extension;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Dashboard\WidgetApi;
+use TYPO3\CMS\Dashboard\Widgets\Interfaces\ChartDataProviderInterface;
+
+class PageviewsPerDay implements ChartDataProviderInterface
+{
+    /**
+     * @var LanguageService
+     */
+    private $languageService;
+
+    /**
+     * @var QueryBuilder
+     */
+    private $queryBuilder;
+
+    /**
+     * @var int
+     */
+    private $days;
+
+    /**
+     * @var array<int>
+     */
+    private $blackListedPages;
+
+    /**
+     * @var string
+     */
+    private $dateFormat;
+
+    public function __construct(
+        LanguageService $languageService,
+        QueryBuilder $queryBuilder,
+        int $days = 31,
+        array $blackListedPages = [],
+        string $dateFormat = 'Y-m-d'
+    ) {
+        $this->queryBuilder = $queryBuilder;
+        $this->days = $days;
+        $this->languageService = $languageService;
+        $this->blackListedPages = $blackListedPages;
+        $this->dateFormat = $dateFormat;
+    }
+
+    public function getChartData(): array
+    {
+        list($labels, $data) = $this->calculateData();
+
+        return [
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => $this->languageService->sL(
+                        Extension::LANGUAGE_PATH . 'widgets.pageViewsBar.chart.dataSet.0'
+                    ),
+                    'backgroundColor' => WidgetApi::getDefaultChartColors()[0],
+                    'border' => 0,
+                    'data' => $data
+                ]
+            ]
+        ];
+    }
+
+    private function calculateData(): array
+    {
+        $labels = [];
+        $data = [];
+
+        for ($daysBefore = $this->days; $daysBefore >= 0; $daysBefore--) {
+            $timeForLabel = strtotime('-' . $daysBefore . ' day');
+            $startPeriod = strtotime('-' . $daysBefore . ' day 0:00:00');
+            $endPeriod =  strtotime('-' . $daysBefore . ' day 23:59:59');
+
+            if ($timeForLabel === false || $startPeriod === false || $endPeriod === false) {
+                continue;
+            }
+
+            $labels[] = date($this->dateFormat, $timeForLabel);
+            $data[] = $this->getPageviewsInPeriod($startPeriod, $endPeriod);
+        }
+
+        return [
+            $labels,
+            $data,
+        ];
+    }
+
+    private function getPageviewsInPeriod(int $start, int $end): int
+    {
+        $constraints = [
+            $this->queryBuilder->expr()->gte('crdate', $start),
+            $this->queryBuilder->expr()->lte('crdate', $end),
+        ];
+
+        if (count($this->blackListedPages)) {
+            $constraints[] = $this->queryBuilder->expr()->notIn(
+                'tx_tracking_pageview.pid',
+                $this->queryBuilder->createNamedParameter(
+                    $this->blackListedPages,
+                    Connection::PARAM_INT_ARRAY
+                )
+            );
+        }
+
+        return (int)$this->queryBuilder
+            ->count('*')
+            ->from('tx_tracking_pageview')
+            ->where(... $constraints)
+            ->execute()
+            ->fetchColumn();
+    }
+}
