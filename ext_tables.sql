1e227deeca04f3f104254e1168f8c79e660d6de2
Daniel Siepmann <coding@daniel-siepmann.de>
2021-08-03 10:06:09 +0200

    Improve performance of widgets with large datasets
    Provide database index for widgets which is used by widget queries to
    fetch data way faster, e.g. 0.1 instead of > 5 seconds in one concrete
    project.
    
    Relates: #63

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,8 @@ CREATE TABLE tx_tracking_pageview (
     user_agent text,
     operating_system varchar(255) DEFAULT '' NOT NULL,
     type int(11) unsigned DEFAULT '0' NOT NULL,
+
+    KEY page_views_per_page (pid,uid,crdate),
 );
 
 CREATE TABLE tx_tracking_recordview (
