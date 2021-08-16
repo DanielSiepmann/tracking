73e2ec0214fd2f1cf9bbd46914a90d101bb990d5
Daniel Siepmann <coding@daniel-siepmann.de>
2021-08-16 10:33:25 +0200

    Add functional tests regarding actual frontend request tracking
    Ensures pageview and recordview are tracked as expected.
    
    Relates: #46

---
+++ Tests/Functional/Fixtures/Extensions/recordview/ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TESTING: Tracking recordview',
+    'description' => 'Used by functional tests',
+    'category' => 'fe',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Daniel Siepmann',
+    'author_email' => 'coding@daniel-siepmann.de',
+    'author_company' => '',
+    'version' => '1.1.2',
+    'constraints' => [
+        'depends' => [
+            'core' => '',
+            'tracking' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
