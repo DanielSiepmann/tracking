6cef2932985d7c697da466d7a4593b6c3cb1af79
Daniel Siepmann <coding@daniel-siepmann.de>
2022-02-02 12:58:05 +0100

    Fix broken performance of list module
    TYPO3 queries information of translations.
    The fix adds database indices which are also applied for tt_content
    by TYPO3 itself.
    This fixes slow loading times of the list module.

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE tx_tracking_pageview (
     type int(11) unsigned DEFAULT '0' NOT NULL,
 
     KEY page_views_per_page (pid,uid,crdate),
+    KEY language (l10n_parent,sys_language_uid),
 );
 
 CREATE TABLE tx_tracking_recordview (
@@ -14,4 +15,7 @@ CREATE TABLE tx_tracking_recordview (
     record varchar(255) DEFAULT '' NOT NULL,
     record_uid int(11) unsigned DEFAULT '0' NOT NULL,
     record_table_name varchar(255) DEFAULT '' NOT NULL,
+
+    KEY record_views_per_page (pid,uid,crdate),
+    KEY language (l10n_parent,sys_language_uid),
 );
